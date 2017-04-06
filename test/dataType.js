b2e8d5ad8872a42bfc05d3941ee2a0bb891b2500
sunhui04 <sunhui04@meituan.com>
2017-04-06 16:45:22 +0800

    add dataType test

---
+++ test/dataType.js
@@ -0,0 +1,18 @@
+/**
+ * @author sunyuhui
+ */
+
+var checkType = require('../source/dataType');
+
+var assert = require('assert');
+
+var dataString = 'sunyuhui';
+
+// check String
+describe('String', function() {
+	describe('checkType', function() {
+		it('should return String when the type of value is String', function() {
+			assert.equal('String', checkType(dataString));
+		});
+	});
+})
