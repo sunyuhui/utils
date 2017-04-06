024750473fc96cec5a34d228a3acafd70946e16d
sunhui04 <sunhui04@meituan.com>
2017-04-06 17:07:14 +0800

    optimize code

--- test/dataType.js
+++ test/dataType.js
@@ -11,12 +11,10 @@ var should = require('chai').should();
 var dataString = 'sunyuhui';
 
 // check String
-describe('String', function() {
-	describe('checkType', function() {
-		it('should return String when the type of value is String', function() {
-			assert.equal('String', checkType(dataString));
-			expect(checkType(dataString)).to.equal('String');
-			checkType(dataString).should.equal('String');
-		});
+describe('check String', function() {
+	it('should return String when the type of value is String', function() {
+		assert.equal('String', checkType(dataString));
+		expect(checkType(dataString)).to.equal('String');
+		checkType(dataString).should.equal('String');
 	});
 })
